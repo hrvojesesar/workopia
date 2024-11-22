7994d872fa306525098a2683af7e7f24c3467e28
hrvojesesar <hrvojesesar28@gmail.com>
2024-11-22 20:44:53 +0100

    Database class and Connection

---
+++ Database.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Database
+{
+    public $conn;
+
+    /**
+     * Constructor for Database class
+     * 
+     * @param string $config
+     */
+    public function __construct($config)
+    {
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+        ];
+
+        try {
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+            echo 'Connected successfully!';
+        } catch (PDOException $e) {
+            throw new Exception("Database connection failed: {$e->getMessage()}");
+        }
+    }
+}
