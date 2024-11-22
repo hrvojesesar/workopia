97e016171c565b727b4acf138ba31bd886ca2799
hrvojesesar <hrvojesesar28@gmail.com>
2024-11-22 18:05:44 +0100

    Router refactor to class

--- public/index.php
+++ public/index.php
@@ -2,8 +2,14 @@
 require '../helpers.php';
 
 
+require basePath('Router.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
 
+$method = $_SERVER['REQUEST_METHOD'];
 
-require basePath('router.php');
+$router->route($uri, $method);
