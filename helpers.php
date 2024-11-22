7dbfbc8053e32772619048e83610802950613769
hrvojesesar <hrvojesesar28@gmail.com>
2024-11-22 14:11:28 +0100

    Created inspect and die functions to checking different values

--- helpers.php
+++ helpers.php
@@ -50,3 +50,32 @@ function loadPartial($name, $data = [])
         echo "Partial '{$name} not found!'";
     }
 }
+
+
+/**
+ * Insert a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ * 
+ */
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Insert a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ * 
+ */
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
