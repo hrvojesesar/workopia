6b6f3d868fd5bf187e436eb9f23c81235cdaa842
hrvojesesar <hrvojesesar28@gmail.com>
2024-11-25 21:43:25 +0100

    Insert Listings

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -85,6 +85,7 @@ class ListingController
         $requiredFields = [
             'title',
             'description',
+            'salary',
             'email',
             'city',
             'state',
@@ -103,7 +104,33 @@ class ListingController
             loadView('listings/create', ['errors' => $errors, 'listing' => $newListingData]);
             return;
         } else {
-            echo "Success";
+            // $this->db->query('INSERT INTO listings (title, description, salary, tags, company, address, city, state, phone, email, requirements, benefits, user_id) VALUES (:title, :description, :salary, :tags, :company, :address, :city, :state, :phone, :email, :requirements, :benefits, :user_id)', $newListingData);
+
+            $fields = [];
+
+            foreach ($newListingData as $field => $value) {
+                $fields[] = $field;
+            }
+
+            $fields = implode(', ', $fields);
+
+            $values = [];
+
+            foreach ($newListingData as $field => $value) {
+                if ($value === '') {
+                    $newListingData[$field] = null;
+                }
+                $values[] = ':' . $field;
+            }
+
+            $values = implode(', ', $values);
+
+
+            $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+
+            $this->db->query($query, $newListingData);
+
+            redirect('/listings');
         }
     }
 }
