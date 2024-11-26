4ff9896de7d173ee52dd968eb115db717d11e1d0
hrvojesesar <hrvojesesar28@gmail.com>
2024-11-26 10:32:13 +0100

    Update Listing Functionality

--- routes.php
+++ routes.php
@@ -7,3 +7,5 @@ $router->get('/listings/create', 'ListingController@create');
 $router->get('/listings/{id}', 'ListingController@show');
 $router->post('/listings', 'ListingController@store');
 $router->delete('/listings/{id}', 'ListingController@destroy');
+$router->get('/listings/edit/{id}', 'ListingController@edit');
+$router->put('/listings/{id}', 'ListingController@update');
